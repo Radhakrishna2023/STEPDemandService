921f838c2d7049af1a36d84663dcb651b61532c2
Radhakrishna2023 <radhakrishna.karumanchi@enquero.com>
2017-04-26 17:07:42 +0530

    Add files via upload

---
+++ src/com/enquero/step/bo/DemandForm.java
@@ -0,0 +1,254 @@
+package com.enquero.step.bo;
+
+public class DemandForm {
+	
+	protected String subject;
+    protected String requestOwner;
+    protected String requestDate;
+    protected String primaryBusinessContact;
+    protected String regionsInScope;
+    protected String priority;
+    protected String targetDueDate;
+    protected String uatTesters;
+    protected String businessObjective;
+    protected String expectedResult;
+    protected String areaofImpact;
+    protected String subArea;
+    protected String communication;
+    protected String valueAdd;
+    protected String businessRoi;
+    protected String risk;
+    protected String qualityofWork;
+    protected String demandIntakeId;
+    protected String statusFlag;
+    /**
+	 * @return the subject
+	 */
+	public String getSubject() {
+		return subject;
+	}
+	/**
+	 * @param subject the subject to set
+	 */
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+	/**
+	 * @return the requestOwner
+	 */
+	public String getRequestOwner() {
+		return requestOwner;
+	}
+	/**
+	 * @param requestOwner the requestOwner to set
+	 */
+	public void setRequestOwner(String requestOwner) {
+		this.requestOwner = requestOwner;
+	}
+	/**
+	 * @return the requestDate
+	 */
+	public String getRequestDate() {
+		return requestDate;
+	}
+	/**
+	 * @param requestDate the requestDate to set
+	 */
+	public void setRequestDate(String requestDate) {
+		this.requestDate = requestDate;
+	}
+	/**
+	 * @return the primaryBusinessContact
+	 */
+	public String getPrimaryBusinessContact() {
+		return primaryBusinessContact;
+	}
+	/**
+	 * @param primaryBusinessContact the primaryBusinessContact to set
+	 */
+	public void setPrimaryBusinessContact(String primaryBusinessContact) {
+		this.primaryBusinessContact = primaryBusinessContact;
+	}
+	/**
+	 * @return the regionsInScope
+	 */
+	public String getRegionsInScope() {
+		return regionsInScope;
+	}
+	/**
+	 * @param regionsInScope the regionsInScope to set
+	 */
+	public void setRegionsInScope(String regionsInScope) {
+		this.regionsInScope = regionsInScope;
+	}
+	/**
+	 * @return the priority
+	 */
+	public String getPriority() {
+		return priority;
+	}
+	/**
+	 * @param priority the priority to set
+	 */
+	public void setPriority(String priority) {
+		this.priority = priority;
+	}
+	/**
+	 * @return the targetDueDate
+	 */
+	public String getTargetDueDate() {
+		return targetDueDate;
+	}
+	/**
+	 * @param targetDueDate the targetDueDate to set
+	 */
+	public void setTargetDueDate(String targetDueDate) {
+		this.targetDueDate = targetDueDate;
+	}
+	/**
+	 * @return the uatTesters
+	 */
+	public String getUatTesters() {
+		return uatTesters;
+	}
+	/**
+	 * @param uatTesters the uatTesters to set
+	 */
+	public void setUatTesters(String uatTesters) {
+		this.uatTesters = uatTesters;
+	}
+	/**
+	 * @return the businessObjective
+	 */
+	public String getBusinessObjective() {
+		return businessObjective;
+	}
+	/**
+	 * @param businessObjective the businessObjective to set
+	 */
+	public void setBusinessObjective(String businessObjective) {
+		this.businessObjective = businessObjective;
+	}
+	/**
+	 * @return the expectedResult
+	 */
+	public String getExpectedResult() {
+		return expectedResult;
+	}
+	/**
+	 * @param expectedResult the expectedResult to set
+	 */
+	public void setExpectedResult(String expectedResult) {
+		this.expectedResult = expectedResult;
+	}
+	/**
+	 * @return the areaofImpact
+	 */
+	public String getAreaofImpact() {
+		return areaofImpact;
+	}
+	/**
+	 * @param areaofImpact the areaofImpact to set
+	 */
+	public void setAreaofImpact(String areaofImpact) {
+		this.areaofImpact = areaofImpact;
+	}
+	/**
+	 * @return the subArea
+	 */
+	public String getSubArea() {
+		return subArea;
+	}
+	/**
+	 * @param subArea the subArea to set
+	 */
+	public void setSubArea(String subArea) {
+		this.subArea = subArea;
+	}
+	/**
+	 * @return the communication
+	 */
+	public String getCommunication() {
+		return communication;
+	}
+	/**
+	 * @param communication the communication to set
+	 */
+	public void setCommunication(String communication) {
+		this.communication = communication;
+	}
+	/**
+	 * @return the valueAdd
+	 */
+	public String getValueAdd() {
+		return valueAdd;
+	}
+	/**
+	 * @param valueAdd the valueAdd to set
+	 */
+	public void setValueAdd(String valueAdd) {
+		this.valueAdd = valueAdd;
+	}
+	/**
+	 * @return the businessRoi
+	 */
+	public String getBusinessRoi() {
+		return businessRoi;
+	}
+	/**
+	 * @param businessRoi the businessRoi to set
+	 */
+	public void setBusinessRoi(String businessRoi) {
+		this.businessRoi = businessRoi;
+	}
+	/**
+	 * @return the risk
+	 */
+	public String getRisk() {
+		return risk;
+	}
+	/**
+	 * @param risk the risk to set
+	 */
+	public void setRisk(String risk) {
+		this.risk = risk;
+	}
+	/**
+	 * @return the qualityofWork
+	 */
+	public String getQualityofWork() {
+		return qualityofWork;
+	}
+	/**
+	 * @param qualityofWork the qualityofWork to set
+	 */
+	public void setQualityofWork(String qualityofWork) {
+		this.qualityofWork = qualityofWork;
+	}
+	/**
+	 * @return the demandIntakeId
+	 */
+	public String getDemandIntakeId() {
+		return demandIntakeId;
+	}
+	/**
+	 * @param demandIntakeId the demandIntakeId to set
+	 */
+	public void setDemandIntakeId(String demandIntakeId) {
+		this.demandIntakeId = demandIntakeId;
+	}
+	/**
+	 * @return the statusFlag
+	 */
+	public String getStatusFlag() {
+		return statusFlag;
+	}
+	/**
+	 * @param statusFlag the statusFlag to set
+	 */
+	public void setStatusFlag(String statusFlag) {
+		this.statusFlag = statusFlag;
+	}
+	
+
+}
---
+++ src/com/enquero/step/bo/ItForm.java
@@ -0,0 +1,163 @@
+package com.enquero.step.bo;
+
+public class ItForm {
+	
+	protected String demandIntakeId;
+	protected String ticketNumber;
+	protected String requestType;
+	protected String estimatedEffort;
+	protected String targetDate;
+	protected String deadLineDate;
+	protected String fundsAvailable;
+	protected String headCount;
+	protected String headCountType;
+	protected String impactedTeams;
+	protected String ticketLifeCycle;
+	protected String requestDate;
+	/**
+	 * @return the demandIntakeId
+	 */
+	public String getDemandIntakeId() {
+		return demandIntakeId;
+	}
+	/**
+	 * @param demandIntakeId the demandIntakeId to set
+	 */
+	public void setDemandIntakeId(String demandIntakeId) {
+		this.demandIntakeId = demandIntakeId;
+	}
+	/**
+	 * @return the ticketNumber
+	 */
+	public String getTicketNumber() {
+		return ticketNumber;
+	}
+	/**
+	 * @param ticketNumber the ticketNumber to set
+	 */
+	public void setTicketNumber(String ticketNumber) {
+		this.ticketNumber = ticketNumber;
+	}
+	/**
+	 * @return the requestType
+	 */
+	public String getRequestType() {
+		return requestType;
+	}
+	/**
+	 * @param requestType the requestType to set
+	 */
+	public void setRequestType(String requestType) {
+		this.requestType = requestType;
+	}
+	/**
+	 * @return the estimatedEffort
+	 */
+	public String getEstimatedEffort() {
+		return estimatedEffort;
+	}
+	/**
+	 * @param estimatedEffort the estimatedEffort to set
+	 */
+	public void setEstimatedEffort(String estimatedEffort) {
+		this.estimatedEffort = estimatedEffort;
+	}
+	/**
+	 * @return the targetDate
+	 */
+	public String getTargetDate() {
+		return targetDate;
+	}
+	/**
+	 * @param targetDate the targetDate to set
+	 */
+	public void setTargetDate(String targetDate) {
+		this.targetDate = targetDate;
+	}
+	/**
+	 * @return the deadLineDate
+	 */
+	public String getDeadLineDate() {
+		return deadLineDate;
+	}
+	/**
+	 * @param deadLineDate the deadLineDate to set
+	 */
+	public void setDeadLineDate(String deadLineDate) {
+		this.deadLineDate = deadLineDate;
+	}
+	/**
+	 * @return the fundsAvailable
+	 */
+	public String getFundsAvailable() {
+		return fundsAvailable;
+	}
+	/**
+	 * @param fundsAvailable the fundsAvailable to set
+	 */
+	public void setFundsAvailable(String fundsAvailable) {
+		this.fundsAvailable = fundsAvailable;
+	}
+	/**
+	 * @return the headCount
+	 */
+	public String getHeadCount() {
+		return headCount;
+	}
+	/**
+	 * @param headCount the headCount to set
+	 */
+	public void setHeadCount(String headCount) {
+		this.headCount = headCount;
+	}
+	/**
+	 * @return the headCountType
+	 */
+	public String getHeadCountType() {
+		return headCountType;
+	}
+	/**
+	 * @param headCountType the headCountType to set
+	 */
+	public void setHeadCountType(String headCountType) {
+		this.headCountType = headCountType;
+	}
+	/**
+	 * @return the impactedTeams
+	 */
+	public String getImpactedTeams() {
+		return impactedTeams;
+	}
+	/**
+	 * @param impactedTeams the impactedTeams to set
+	 */
+	public void setImpactedTeams(String impactedTeams) {
+		this.impactedTeams = impactedTeams;
+	}
+	/**
+	 * @return the ticketLifeCycle
+	 */
+	public String getTicketLifeCycle() {
+		return ticketLifeCycle;
+	}
+	/**
+	 * @param ticketLifeCycle the ticketLifeCycle to set
+	 */
+	public void setTicketLifeCycle(String ticketLifeCycle) {
+		this.ticketLifeCycle = ticketLifeCycle;
+	}
+	/**
+	 * @return the requestDate
+	 */
+	public String getRequestDate() {
+		return requestDate;
+	}
+	/**
+	 * @param requestDate the requestDate to set
+	 */
+	public void setRequestDate(String requestDate) {
+		this.requestDate = requestDate;
+	}
+	
+
+}
